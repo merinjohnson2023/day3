48c9a4889351485cbfd50197c02544c2d6933bb8
merinjohnson2023 <153511617+merinjohnson2023@users.noreply.github.com>
2023-12-14 09:57:31 +0530

    n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HerosectionComponent } from './herosection/herosection.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NavbarComponent,HerosectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'day3';
+
+  name:string='merin';
+
+  changenew(){
+    alert('The name is'+this.name)
+  }
+
+districts: any=[
+  {
+    id:1,
+    title:"Trivandrum",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwVHoJl-ltXPYzewI9eGLk_ctf8_JX7wdeulCITRFd9Q&s"
+  }
+];
+
+
+
+
+
+
+
 }
